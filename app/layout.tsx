8f2a15e9a883cf862749931d9d8018bf07579f97
yashi-shukla <yashishkl1@gmail.com>
2026-02-10 03:47:45 +0530

    add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import localFont from "next/font/local";
 import { Inter, Instrument_Serif, Playfair_Display } from "next/font/google";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { SmoothScroll } from "@/components/SmoothScroll";
 
@@ -88,6 +89,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "vevr0ki4bk");
+        `}
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} ${playfairDisplay.variable} ${inter.variable} antialiased font-sans`}
       >
